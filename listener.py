ab125c5761057d1c79f6cd77fd68140d8bda1dea
Sen Harald Hjort Inao <senharald07@gmail.com>
2024-07-19 02:19:09 +0200

    made the keyboard listener

---
+++ listener.py
@@ -0,0 +1,52 @@
+import win32api
+import threading
+from viritualKeys import virtual_keys
+
+class Event:
+    def __init__(self, vkey) -> None:
+        self.vkey = vkey
+        self.string = virtual_keys[vkey]
+
+    def __str__(self) -> str:
+        return self.string
+
+class Listener:
+    def __init__(self, eventHandler=None) -> None:
+        self.listening = False
+        self.listener = None
+        self.eventHandler = eventHandler
+        self.keystates = {vkey:False for vkey in virtual_keys.keys()}
+
+    def isPressed(self, key:int):
+        return win32api.GetAsyncKeyState(key)
+
+    def listen(self):
+        if (not self.listening):
+            self.listening = True
+            self.listenerThread = threading.Thread(target=self._start)
+            self.listenerThread.start()
+
+    def stop(self):
+        if (self.listening):
+            self.listening = False
+            self.listenerThread.join()
+
+    def _start(self):
+        while (self.listening):
+            self._updateKeystates()
+
+    def _updateKeystates(self):
+        keystates = self.keystates
+        newDict = keystates.copy()
+        for key in keystates.keys():
+            if (self.isPressed(key)):
+                if (not newDict[key]):
+                    if (self.eventHandler):
+                        event = Event(key)
+                        self.eventHandler(event)
+
+                newDict[key] = True
+            else:
+                newDict[key] = False
+
+        self.keystates = newDict
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from keylogger import Keylogger
+from listener import Listener
+import time
 
+def eventHandler(event):
+    print(event, event.vkey)
 
 def main():
-    keylogger = Keylogger()
+    listener = Listener(eventHandler)
+    listener.listen()
 
-    keylogger.start()
+    try:
+        while True:
+            if (listener.isPressed(0x1b)):
+                print("ESC pressed, Quitting")
+                listener.stop()
+                break
+
+            time.sleep(0.00001)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt , Quitting")
+        listener.stop()
 
 if __name__ == "__main__":
     main()
